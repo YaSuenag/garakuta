e9f59b60b648f2c5b433af9aca40107d994f50d5
Yasumasa Suenaga <yasuenag@gmail.com>
2026-01-17 16:08:35 +0900

    Add function metadata to NativeOverflow

--- NativeStackOverflow/NativeOverflow-aarch64.s
+++ NativeStackOverflow/NativeOverflow-aarch64.s
@@ -2,9 +2,12 @@
 .type Java_NativeStackOverflow_doOverflow, @function
 
 Java_NativeStackOverflow_doOverflow:
+.cfi_startproc
   stp x29, x30, [sp, #-16]
   mov x29, sp
 
   1:
     stp xzr, xzr, [sp, #-16]!
     b 1b
+.cfi_endproc
+.size Java_NativeStackOverflow_doOverflow, .-Java_NativeStackOverflow_doOverflow
--- NativeStackOverflow/NativeOverflow-x86_64.s
+++ NativeStackOverflow/NativeOverflow-x86_64.s
@@ -2,6 +2,7 @@
 .type Java_NativeStackOverflow_doOverflow, @function
 
 Java_NativeStackOverflow_doOverflow:
+.cfi_startproc
   push %rbp
   movq %rsp, %rbp
   xorq %rax, %rax
@@ -10,3 +11,5 @@ Java_NativeStackOverflow_doOverflow:
     movq %rax, (%rsp)
     subq $0x8, %rsp
     jmp 1b
+.cfi_endproc
+.size Java_NativeStackOverflow_doOverflow, .-Java_NativeStackOverflow_doOverflow
